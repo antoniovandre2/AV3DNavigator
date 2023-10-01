cea7b8f13411b5a67fd1fca77d7d62c4c115b73e
Antonio Vandré Pedrosa Furtunato Gomes <90938909+antoniovandre2@users.noreply.github.com>
2023-10-01 09:55:44 -0300

    Add files via upload

--- Espaços/AV3DNavigator - Espaço - Elipsoide de revolução.c
+++ Espaços/AV3DNavigator - Espaço - Elipsoide de revolução.c
@@ -14,14 +14,14 @@ Arquivo gerador do "AV3DNavigator - Espaço - Elipsoide de revolução.txt".
 int main ()
     {
     int resolucao = 12;
-    float a = 3;
+    float a = 4;
     float b = 2;
 
     for (int i = 0; i < resolucao; i++)
         for (int j = 0; j < resolucao; j++)
             {
-            printf("%f,%f,%f;%f,%f,%f|", b * sin(j * 2 * M_PI / resolucao), a * cos(j * 2 * M_PI / resolucao) * b * cos(i * 2 * M_PI / resolucao), b * sin(i * 2 * M_PI / resolucao), b * sin((j + 1) * 2 * M_PI / resolucao), a * cos((j + 1) * 2 * M_PI / resolucao) * b * cos(i * 2 * M_PI / resolucao), b * sin(i * 2 * M_PI / resolucao));
+            printf("%f,%f,%f;%f,%f,%f|", b * sin(j * 2 * M_PI / resolucao), a * cos(j * 2 * M_PI / resolucao) * cos(i * 2 * M_PI / resolucao), b * sin(i * 2 * M_PI / resolucao), b * sin((j + 1) * 2 * M_PI / resolucao), a * cos((j + 1) * 2 * M_PI / resolucao) * cos(i * 2 * M_PI / resolucao), b * sin(i * 2 * M_PI / resolucao));
 
-            printf("%f,%f,%f;%f,%f,%f|", b * sin(j * 2 * M_PI / resolucao), a * cos(j * 2 * M_PI / resolucao) * b * cos(i * 2 * M_PI / resolucao), b * sin(i * 2 * M_PI / resolucao), b * sin(j * 2 * M_PI / resolucao), a * cos(j * 2 * M_PI / resolucao) * b * cos((i + 1) * 2 * M_PI / resolucao), b * sin((i + 1) * 2 * M_PI / resolucao));
+            printf("%f,%f,%f;%f,%f,%f|", b * sin(j * 2 * M_PI / resolucao), a * cos(j * 2 * M_PI / resolucao) * cos(i * 2 * M_PI / resolucao), b * sin(i * 2 * M_PI / resolucao), b * sin(j * 2 * M_PI / resolucao), a * cos(j * 2 * M_PI / resolucao) * cos((i + 1) * 2 * M_PI / resolucao), b * sin((i + 1) * 2 * M_PI / resolucao));
             }
     }
