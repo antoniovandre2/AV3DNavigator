acefdc1c75c7be108837c5c8a4d2dd02000e769c
Antonio Vandré Pedrosa Furtunato Gomes <90938909+antoniovandre2@users.noreply.github.com>
2023-10-02 14:09:42 -0300

    Add files via upload

--- Espaços/AV3DNavigator - Espaço - Trombeta de Gabriel.c
+++ Espaços/AV3DNavigator - Espaço - Trombeta de Gabriel.c
@@ -5,7 +5,7 @@ Av3DNavigator: "https://github.com/antoniovandre2/AV3DNavigator".
 
 Arquivo gerador do "AV3DNavigator - Espaço - Trombeta de Gabriel.txt".
 
-Última atualização: 01-10-2023.
+Última atualização: 02-10-2023.
 */
 
 #include <stdio.h>
@@ -20,7 +20,7 @@ int main ()
     for (int i = 1; i < resolucao; i++)
         for (int j = 0; j < resolucao; j++)
             {
-            if (1 != resolucao - 1) printf("%f,%f,%f;%f,%f,%f|", a / (i * comprimento / resolucao) * cos(j * 2 * M_PI / resolucao), i * comprimento / resolucao - comprimento / 2, a / (i * comprimento / resolucao) * sin(j * 2 * M_PI / resolucao), a / ((i + 1) * comprimento / resolucao) * cos(j * 2 * M_PI / resolucao), (i + 1) * comprimento / resolucao - comprimento / 2, a / ((i + 1) * comprimento / resolucao) * sin(j * 2 * M_PI / resolucao));
+            if (i != resolucao - 1) printf("%f,%f,%f;%f,%f,%f|", a / (i * comprimento / resolucao) * cos(j * 2 * M_PI / resolucao), i * comprimento / resolucao - comprimento / 2, a / (i * comprimento / resolucao) * sin(j * 2 * M_PI / resolucao), a / ((i + 1) * comprimento / resolucao) * cos(j * 2 * M_PI / resolucao), (i + 1) * comprimento / resolucao - comprimento / 2, a / ((i + 1) * comprimento / resolucao) * sin(j * 2 * M_PI / resolucao));
 
             printf("%f,%f,%f;%f,%f,%f|", a / (i * comprimento / resolucao) * cos(j * 2 * M_PI / resolucao), i * comprimento / resolucao - comprimento / 2, a / (i * comprimento / resolucao) * sin(j * 2 * M_PI / resolucao), a / (i * comprimento / resolucao) * cos((j + 1) * 2 * M_PI / resolucao), i * comprimento / resolucao - comprimento / 2, a / (i * comprimento / resolucao) * sin((j + 1) * 2 * M_PI / resolucao));
             }
