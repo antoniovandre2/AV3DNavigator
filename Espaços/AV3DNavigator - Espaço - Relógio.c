0782653094cce5e3da1fb7bd287e5cd7e7094362
Antonio Vandré Pedrosa Furtunato Gomes <90938909+antoniovandre2@users.noreply.github.com>
2024-08-15 15:52:26 -0300

    Novo tipo de espaço com parâmetros de tempo.

---
+++ Espaços/AV3DNavigator - Espaço - Relógio.c
@@ -0,0 +1,53 @@
+/*
+Proprietário: Antonio Vandré Pedrosa Furtunato Gomes (bit.ly/antoniovandre_legadoontologico)
+
+Av3DNavigator: "https://github.com/antoniovandre2/AV3DNavigator".
+
+Arquivo gerador do "AV3DNavigator - Espaço - Relógio.txt".
+
+Última atualização: -15-08-2024. Não considerando alterações em variáveis globais.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main (int argc, char * argv[])
+	{
+	int resolucao = 5;
+	long double raiosegundos = 4;
+	long double raiominutos = 3;
+	long double raiohoras = 2;
+	long double espessura = 1;
+	int i;
+
+	printf("@");
+
+	for (i = 0; i < resolucao; i++)
+		{
+		printf("0divisor%Lf%s%Lf) + %Lf)divisor%Lf%s%Lf) + %Lf)DIVISOR0divisor%Lf%s%Lf) - %Lf)divisor%Lf%s%Lf) - %Lf)DIVISOR", espessura / 2, " * cos((AV3DNPTS * ", (long double) M_PI / 30, (long double) M_PI_2, espessura / 2, " * sin((AV3DNPTS * ", (long double) M_PI / 30, (long double) M_PI_2, espessura / 2, " * cos((AV3DNPTS * ", (long double) M_PI / 30, (long double) M_PI_2, espessura / 2, " * sin((AV3DNPTS * ", (long double) M_PI / 30, (long double) M_PI_2);
+
+		fflush(stdout);
+
+		printf("0divisor%Lf%s%Lf)divisor%Lf%s%Lf)color255divisor0divisor0|", raiosegundos, " * cos(AV3DNPTS * ", (long double) M_PI / 30, raiosegundos, " * sin(AV3DNPTS * ", (long double) M_PI / 30);
+
+		fflush(stdout);
+
+		printf("-0.5divisor%Lf%s%Lf) + %Lf)divisor%Lf%s%Lf) + %Lf)DIVISOR-0.5divisor%Lf%s%Lf) - %Lf)divisor%Lf%s%Lf) - %Lf)DIVISOR", espessura / 2, " * cos((AV3DNPTM * ", (long double) M_PI / 30, (long double) M_PI_2, espessura / 2, " * sin((AV3DNPTM * ", (long double) M_PI / 30, (long double) M_PI_2, espessura / 2, " * cos((AV3DNPTM * ", (long double) M_PI / 30, (long double) M_PI_2, espessura / 2, " * sin((AV3DNPTM * ", (long double) M_PI / 30, (long double) M_PI_2);
+
+		fflush(stdout);
+
+		printf("-0.5divisor%Lf%s%Lf)divisor%Lf%s%Lf)color0divisor255divisor0|", raiominutos, " * cos(AV3DNPTM * ", (long double) M_PI / 30, raiominutos, " * sin(AV3DNPTM * ", (long double) M_PI / 30);
+
+		fflush(stdout);
+
+		printf("-1divisor%Lf%s%Lf) + %Lf)divisor%Lf%s%Lf) + %Lf)DIVISOR-1divisor%Lf%s%Lf) - %Lf)divisor%Lf%s%Lf) - %Lf)DIVISOR", espessura / 2, " * cos((AV3DNPTH * ", (long double) M_PI / 30, (long double) M_PI_2, espessura / 2, " * sin((AV3DNPTH * ", (long double) M_PI / 30, (long double) M_PI_2, espessura / 2, " * cos((AV3DNPTH * ", (long double) M_PI / 30, (long double) M_PI_2, espessura / 2, " * sin((AV3DNPTH * ", (long double) M_PI / 30, (long double) M_PI_2);
+
+		fflush(stdout);
+
+		printf("-1divisor%Lf%s%Lf)divisor%Lf%s%Lf)color255divisor255divisor0|", raiohoras, " * cos(AV3DNPTH * ", (long double) M_PI / 30, raiominutos, " * sin(AV3DNPTH * ", (long double) M_PI / 30);
+
+		fflush(stdout);
+		}
+	}
